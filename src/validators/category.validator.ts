29f7b3715982a8431442a240b1273035290dab63
ThyDevourer <pedro@ivanpartida.xyz>
2022-08-16 21:07:38 -0700

    Formatting

--- src/validators/category.validator.ts
+++ src/validators/category.validator.ts
@@ -2,8 +2,13 @@ import { BodyValidator } from 'fastest-express-validator'
 import { Request, Response, NextFunction } from 'express'
 
 export const addCategoryValidator = BodyValidator({
-  name: { type: 'string' },
-  active: { type: 'boolean', default: true }
+  name: {
+    type: 'string'
+  },
+  active: {
+    type: 'boolean',
+    default: true
+  }
 }, (err, req: Request, res: Response, next: NextFunction) => {
   console.log('Validation error!')
   console.log(err)
--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -3,16 +3,44 @@ import { Request, Response, NextFunction } from 'express'
 import { roles } from '../models/user.model'
 
 export const addUserValidator = BodyValidator({
-  name: { type: 'string' },
-  lastName: { type: 'string' },
-  username: { type: 'string' },
-  email: { type: 'string' },
-  password: { type: 'string' },
-  role: { type: 'string', enum: roles, default: 'normal' },
-  active: { type: 'boolean', default: true },
-  verified: { type: 'boolean', default: false },
-  registeredAt: { type: 'date', convert: true, default: new Date() },
-  lastLogin: { type: 'date', convert: true, optional: true }
+  name: {
+    type: 'string'
+  },
+  lastName: {
+    type: 'string'
+  },
+  username: {
+    type: 'string'
+  },
+  email: {
+    type: 'string'
+  },
+  password: {
+    type: 'string'
+  },
+  role: {
+    type: 'string',
+    enum: roles,
+    default: 'normal'
+  },
+  active: {
+    type: 'boolean',
+    default: true
+  },
+  verified: {
+    type: 'boolean',
+    default: false
+  },
+  registeredAt: {
+    type: 'date',
+    convert: true,
+    default: new Date()
+  },
+  lastLogin: {
+    type: 'date',
+    convert: true,
+    optional: true
+  }
 }, (err, req: Request, res: Response, next: NextFunction) => {
   next(err)
 })
