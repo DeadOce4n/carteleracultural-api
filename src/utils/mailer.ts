43df1ac22465997418ea11842b6a0337849cdb53
ThyDevourer <pedro@ivanpartida.xyz>
2022-10-09 13:31:00 -0700

    refactor: configure options via env

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -5,11 +5,13 @@ const host = process.env.MAIL_HOST ?? 'localhost'
 const port = Number(process.env.MAIL_PORT ?? 2525)
 const user = process.env.MAIL_USER ?? 'user'
 const pass = process.env.MAIL_PASS ?? 'pass'
+const secure = process.env.MAIL_SECURE === 'true'
+const senderAddress = process.env.MAIL_SENDER_ADDRESS ?? 'verify@carteleraculturalens.com'
 
 const transporter = createTransport({
   host,
   port,
-  secure: process.env.NODE_ENV === 'production',
+  secure,
   auth: {
     user,
     pass
@@ -20,7 +22,7 @@ export const sendMail = async (email: string, code: string) => {
   logger.logger.info(`Sending verification email to ${email}`)
 
   const info = await transporter.sendMail({
-    from: 'verification@carteleraculturalens.com',
+    from: senderAddress,
     to: email,
     subject: 'Please verify your account',
     html: `Verification code: ${code}`
