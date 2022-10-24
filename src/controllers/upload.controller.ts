1d4b0e8bb6ef5547a18da79827c05452b0be3ad4
ThyDevourer <pedro@ivanpartida.xyz>
2022-10-23 20:00:06 -0700

    feat: make uploaded images squared

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -1,11 +1,32 @@
+import fs from 'fs/promises'
 import { Request, Response, NextFunction } from 'express'
+import Sharp from 'sharp'
 
-export const imageUpload = (req: Request, res: Response, next: NextFunction) => {
-  return res.status(200).send({
-    data: req.file?.path ?? '',
-    meta: {
-      success: true,
-      message: 'File uploaded successfully!'
-    }
-  })
+export const imageUpload = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const filePath = req.file!.path
+    const image = Sharp(filePath)
+    const stats = await image.stats()
+    const extension = filePath.split('.').at(-1) as string
+    const newFilePath = `${filePath.replace('.' + extension, '')}-sq.${extension}`
+
+    await image.resize({
+      width: 600,
+      height: 600,
+      fit: 'contain',
+      background: stats.dominant
+    }).toFile(newFilePath)
+
+    await fs.unlink(filePath)
+
+    return res.status(200).send({
+      data: newFilePath,
+      meta: {
+        success: true,
+        message: 'File uploaded successfully!'
+      }
+    })
+  } catch (e) {
+    next(e)
+  }
 }
