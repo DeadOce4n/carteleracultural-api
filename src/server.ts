ccdcf555b82284d7f0b7c375f2d282db1b49d7b3
ThyDevourer <pedro@ivanpartida.xyz>
2022-11-03 20:11:01 -0700

    feat(sockets): use sockets for notifications

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import app from './app'
 import { logger } from './utils/logger'
+import http from 'http'
+import { Server } from 'socket.io'
 
 const port = Number(process.env.PORT ?? 3000)
-
-app.listen(port, () => {
+const server = http.createServer(app).listen(port, () => {
   logger.logger.info(`Server is running at http://localhost:${port}`)
+})
+export const io = new Server(server, {
+  cors: {
+    origin: process.env.CORS_ORIGINS?.split(',') ?? ['http://localhost:5173']
+  }
+})
+io.on('connection', socket => {
+  logger.logger.info('Connected!', socket.id)
 })
